030c1526682ee8ad09d28e1f4b9663812837aa63
Kha Tran <khatran961217@gmail.com>
2022-09-23 18:59:52 +1000

    commit-2
    Installed react-router-dom to navigate between pages
    Setup routing in the App.js
    Created Dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 import React from 'react'
+import Dashboard from './Dashboard'
 import Signup from './Signup'
 import { Container } from "react-bootstrap"
 import { AuthProvider } from '../contexts/AuthContext'
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom' // react-router-dom
 
 function App() {
   return (
-    <AuthProvider>
+  
       <Container 
         className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}>
 
         <div className="w-100" style={{ maxWidth: '400px' }}>
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route exact path="/" element={<Dashboard/>} />
+                <Route path="/signup" element={<Signup/>} />
+              </Routes>
+            </AuthProvider>
+          </Router>
         </div>
 
       </Container>
-    </AuthProvider>
     
   )
 }
