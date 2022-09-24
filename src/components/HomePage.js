e488143fe870586ac0e393061953040cbe061ccf
Kha Tran <khatran961217@gmail.com>
2022-09-24 19:22:50 +1000

    Squashed commit of the following:
    commit ee7878e60c7f64370972c1a3f92ea95494421ba1
    Author: Kha Tran <khatran961217@gmail.com>
    Date:   Sat Sep 24 19:22:12 2022 +1000
    
        commit-1
    
        Created Homepage for teacher and student access
        Updated Log In page
        Installed react-icons package

---
+++ src/components/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+const HomePage = () => {
+    return (
+        <div className="text-center">
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Welcome to Stray Balloon</h2>
+
+                    <div className="w-100 text-center mt-3">
+                    <Button><Link style={{color: 'white', textDecoration: 'none'}} to="/login">Teacher</Link></Button>
+                    </div>
+
+                    <div className="w-100 text-center mt-3">
+                        <Button><Link style={{color: 'white', textDecoration: 'none'}} to="/">Student</Link></Button>
+                    </div>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default HomePage;
