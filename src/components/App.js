fc8544c3dd71f2ae7adbe797fb8f1036041627d1
Kha Tran <khatran961217@gmail.com>
2022-10-07 16:30:43 +1100

    Squashed commit of the following:
    commit 574a749956f10835587924d7bbcf550ec37fa023
    Author: Nick-n1 <114322649+Nick-n1@users.noreply.github.com>
    Date:   Fri Oct 7 16:24:18 2022 +1100
    
        EnterGameCode
    
        Add link to the EnterGameCode Page on homepage.js
    
        add background will be done in another task

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,41 @@
-import React from 'react'
-import HomePage from './HomePage'
-import Dashboard from './Dashboard'
-import Signup from './Signup'
-import Login from './Login'
-import UpdateProfile from './UpdateProfile'
-import ForgotPassword from './ForgotPassword'
-import PrivateRoutes from './PrivateRoutes'
-import { Container } from "react-bootstrap"
-import { AuthProvider } from '../contexts/AuthContext'
-import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom' // react-router-dom
+import React from "react";
+import HomePage from "./HomePage";
+import Dashboard from "./Dashboard";
+import Signup from "./Signup";
+import Login from "./Login";
+import EnterGameCode from "./EnterGameCode";
+import UpdateProfile from "./UpdateProfile";
+import ForgotPassword from "./ForgotPassword";
+import PrivateRoutes from "./PrivateRoutes";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // react-router-dom
 
 function App() {
   return (
-      <Container 
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}>
-
-        <div className="w-100" style={{ maxWidth: '400px' }}>
-          <Router>
-            <AuthProvider>
-
-              <Routes>
-                <Route exact path="/" element={<HomePage/>} />
-                <Route element={<PrivateRoutes />}>
-                  <Route path="dashboard" element={<Dashboard/>} />
-                  <Route path="/update-profile" element={<UpdateProfile/>} />
-                </Route>
-                
-                <Route path="/signup" element={<Signup/>} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="/forgot-password" element={<ForgotPassword/>} />
-              </Routes>
-            </AuthProvider>
-          </Router>
-        </div>
-
-      </Container>
-  )
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route element={<PrivateRoutes />}>
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="/update-profile" element={<UpdateProfile />} />
+              </Route>
+              <Route path="/entergamecode" element={<EnterGameCode />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  );
 }
 
-export default App
+export default App;
