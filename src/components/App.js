f53a0b8ab5f9285d23d63c3bf42a727137dc4f84
Kha Tran <khatran961217@gmail.com>
2022-10-20 22:36:02 +1100

    commit 2: Display player names
    Used uuid to create random unique student id.
    Transfer student to game lobby after they provide player name and correct gamecode

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HomePage from "./HomePage";
 import Dashboard from "./Dashboard";
 import GameLobby from "./GameLobby";
@@ -10,6 +10,7 @@ import ForgotPassword from "./ForgotPassword";
 import PrivateRoutes from "./PrivateRoutes";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
+import { PlayersProvider } from "../contexts/PlayersProvider"
 import useLocalStorage from '../hooks/useLocalStorage';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // react-router-dom
 
@@ -24,18 +25,20 @@ function App() {
       <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
           <AuthProvider>
+            <PlayersProvider>
             <Routes>
               <Route exact path="/" element={<HomePage />} />
               <Route element={<PrivateRoutes />}>
                 <Route path="dashboard" element={<Dashboard gamecode={setGamecode}/>} />
                 <Route path="/update-profile" element={<UpdateProfile />} />
-                <Route path="/game-lobby" element={<GameLobby gamecode={gamecode}/>} />
+                <Route path="/game-lobby" element={<GameLobby gamecode={gamecode} />} />
               </Route>
-              <Route path="/student" element={<StudentAccess />} />
+              <Route path="/student" element={<StudentAccess gamecode={gamecode} />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/login" element={<Login />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
             </Routes>
+            </PlayersProvider>
           </AuthProvider>
         </Router>
       </div>
--- src/components/StudentAccess.js
+++ src/components/StudentAccess.js
@@ -1,44 +1,33 @@
 import React, { useRef, useState } from "react";
-import {
-  Form,
-  Button,
-  Card,
-  Alert,
-  Container,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { usePlayers } from '../contexts/PlayersProvider'
+import { Form, Button, Card, Alert, Container, Row, Col } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
+import { v4 as uuidV4 } from 'uuid'
 
-export default function EnterGameCode() {
+export default function StudentAccess({gamecode}) {
   // Initialisers
   const gamecodeRef = useRef();
   const nameRef = useRef();
+  const { addPlayer } = usePlayers()
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false); // Set to false by default since there is nothing to load
   const navigate = useNavigate(); // To go to lobby after enter the correct game code
+  let pin = gamecode.toString();
 
   function handleSubmit(event) {
     event.preventDefault(); // Prevent the form from refreshing
 
+    // Check if game code match
+    if (gamecodeRef.current.value !== pin) {
+      return setError("Game code does not match! Please check your game code");
+    }
+
     setError(""); // Set to empty string as there is no error yet
     setLoading(true);
-    //EnterGameCode(GameCodeRef.current.value);
-    navigate("/"); // Go to game lobby after enter the correct game code
-
+    addPlayer(uuidV4(), nameRef.current.value)
+    navigate("/game-lobby"); // Go to game lobby after enter the correct game code
     setLoading(false);
-  }
-
-  function checkGamecode() {
-    // Check if game code match
-    if (gamecodeRef.current.value) {
-      return setError("Game Code do not match! Please check your game code");
-    }
-  }
-
-  function checkPlayerName() {
-
   }
 
   return (
